c6cd04519a54b1bdfeb0e10d0ae70c8561276151
Azerg0ifen <florent.vasseur92@gmail.com>
2024-11-09 19:05:16 +0100

    SRC: Ajout des classes FirewallManager et FirewallRule

---
+++ src/FirewallManager.py
@@ -0,0 +1,10 @@
+import subprocess
+
+class FirewallManager:
+    """
+    Classe gérant les règles de pare-feu.
+    
+    Méthodes :
+        - add_rule(self, rule: FirewallRule) : Ajoute une règle au pare-feu.
+    """
+    pass
